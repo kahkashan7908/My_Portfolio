3aae3de669dcd9bd34cf4da07004fd41d9df5395
kahkashan7908 <kahkashankhatoon8@gmail.com>
2023-10-05 21:16:04 +0530

    add portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,231 @@
+export const Bio = {
+    name: "Khakkashan khatoon",
+    roles: [
+      "Full Stack Developer",
+      "Web developer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I'm someone who loves learning and trying new things. I always work hard to do my best and get good results. I stay positive and believe I can keep getting better and do important stuff.",
+    github: "https://github.com/kahkashan7908",
+    resume:"https://drive.google.com/file/d/1BZns_LaFwUjhfSkyoLYC2XogQ7QYGsdU/view?usp=drivesdk",
+    linkedin: "https://www.linkedin.com/in/kahkashan-khatoon-7195691b3/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+      
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "ReactJs",
+          image:"https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+        },
+       
+       
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+       
+        {
+          name: "Python",
+          image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "Django",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/django/django-plain-wordmark.svg",
+        },
+        {
+          name: "Rest Framework",
+          image:
+            "https://storage.caktusgroup.com/media/blog-images/drf-logo2.png",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://cdn4.iconfinder.com/data/icons/logos-3/181/MySQL-256.png",
+        },
+    
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+       
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+       
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://images.thecompanycheck.com//companylogo/iLogitron_Technologies_Pvt_Ltd_LOGO_058a0e5a.webp",
+      role: "Frontend developer",
+      company: "iLogitron Technologies Pvt. Ltd.",
+      date: "Octuber 2022 - December 2022",
+      desc: "Working on Frontend part of website.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Bootstrap",
+      ],
+    
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://bitm.org.in/assets/img/caro12.jpg",
+      school: "Bengal Institute of Technology and Management (BITM), santiniketan",
+      date: "August 2016 - June 2020",
+      grade: "8.25 CGPA",
+      desc: "I completed my B.Tech in Electrical Engineering from Bengal Institute of Technology and Management.",
+      degree: "Bachelor of Technology (BTech) in Electrical Engineering",
+    },
+    {
+      id: 1,
+      img: "https://images.collegedunia.com/public/college_data/images/appImage/3658_RAMKRISH_APP.jpg",
+      school: "RK College, Madhubani(Bihar)",
+      date: "june 2014 - May-2016",
+      grade: "70%",
+      desc: "I completed my high school education (Class 12) at RK College, where I studied Science",
+      degree: "Intermediate Science",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwFuhmnbYl9toB3rdGW_34hTJ81II_XhtFzg&usqp=CAU",
+      school: "High School Koilakh",
+      date: "Apr 2013-June 2014",
+      grade: "77%",
+      desc: "I completed my class 10 education at high School Koilakh.",
+      degree: "Secondary School",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Examinations Result Portal ",
+      date: "May 2023 - May 2023",
+      description:
+        "This is a web-based application that enables students to check their exam results online. New users must create an account by signing up and then log in to the system. The application offers login, logout, and signup features and utilizes the Django web framework with a MySQL database for user data management. Additionally, fake student information has been added to the database using the Faker module. The frontend is designed with HTML, CSS, and JavaScript.",
+      image:
+        "https://user-images.githubusercontent.com/109336765/272915667-1356ddb2-128b-43bb-8120-a49fe07d0043.PNG",
+      tags: ["python", "django", "Mysql", "HTML","CSS","Bootstrap"],
+      category: "web app",
+      github: "https://github.com/kahkashan7908/examination_result_portal",
+      
+    },
+
+
+    {
+      id: 2,
+      title: "Ecommerce_website",
+      date: "july 2023 - july2023",
+      description:
+        "This is a web-based application that Implements the functioning of an E-commerce website that performs various functions like login, logout, registration, add to cart, billing and much more. For backend part, it is using web framework as Python Django and database as sqlite3. The frontend is designed using HTML,CSS and JavaScript.",
+      image:
+        "https://user-images.githubusercontent.com/109336765/272946360-7586107f-346c-4261-b131-f94f37e79a5f.PNG",
+      tags: ["python", "django", "Sqlite3", "Jvascript","HTML","CSS","Bootstrap",],
+      category: "web app",
+      github: "https://github.com/kahkashan7908/Ecommerce_sites",
+      
+    },
+
+    {
+      id: 3,
+      title: "Calorie Trackers",
+      date: "August 2023 - August 2023",
+      description:
+        "Developed a full-stack web application that tracks macro nutrients and calorie intake, and monitors calorie consumption.",
+      image:
+        "https://user-images.githubusercontent.com/109336765/272884962-d3277ef2-9a46-4b53-9746-c23b9ce5003e.PNG",
+      tags: ["python", "django", "Sqlite3", "Javascript", "Chart.Js"],
+      category: "web app",
+      github: "https://github.com/kahkashan7908/Calorie_Tracker/tree/main",
+      
+     
+    },
+
+    {
+      id: 4,
+      title: "Ecommerce Front",
+      date: "Nov 2022",
+      description:"This is a web-based application, and it is a sample frontend page of a Ecommerce. You can view this page by clicking on the 'Live' button.",
+      image:
+        "https://user-images.githubusercontent.com/109336765/272955858-4ce3d106-6428-4a93-b2a7-6fa418c690e1.PNG",
+      tags: ["HTML","CSS","Jvascript"],
+      category: "Frontend",
+      github: "https://github.com/kahkashan7908/E-Commerce-shopping-website",
+      webapp: "https://kahkashan7908.github.io/E-Commerce-shopping-website/",
+    },
+   
+    {
+      id: 5,
+      title: "Food wep app",
+      date: "Nov 2022",
+      description:"This is a web-based application, and it is a sample frontend page of a food app. You can view this page by clicking on the 'Live' button.",
+      image:
+        "https://user-images.githubusercontent.com/109336765/272952048-e9e4c7bb-b85f-4f78-b3c1-a20554230b47.PNG",
+      tags: ["HTML","CSS","Jvascript"],
+      category: "Frontend",
+      github: "https://github.com/kahkashan7908/food-website",
+      webapp: "https://kahkashan7908.github.io/food-website/",
+    },
+   
+   ];
+  
+ 
